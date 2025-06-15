3c567e89967ce299b815a55487db1b02dcc6d7df
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-06-15 10:55:27 +0300

    Added formatting support for sublime text config json files.

--- prettier-jet-service/constants.js
+++ prettier-jet-service/constants.js
@@ -1,38 +1,41 @@
-import { tmpdir } from "node:os";
-import path from "node:path";
+import { tmpdir } from 'node:os';
+import path from 'node:path';
 
 // Cross-platform temp file path
-export const PORT_FILE_PATH = path.join(tmpdir(), "prettierjet.port");
+export const PORT_FILE_PATH = path.join(tmpdir(), 'prettierjet.port');
 
 export const MAX_TXT_SIZE = 1 * 1024 * 1024; // 1MB
 
 export const PRETTIER_SUPPORTED_EXTS = [
-  ".js",
-  ".jsx",
-  ".mjs",
-  ".cjs",
-  ".ts",
-  ".tsx",
-  ".mts",
-  ".cts",
-  ".json",
-  ".json5",
-  ".jsonc",
-  ".html",
-  ".htm",
-  ".xml",
-  ".svg",
-  ".css",
-  ".scss",
-  ".less",
-  ".md",
-  ".markdown",
-  ".mdx",
-  ".yaml",
-  ".yml",
-  ".toml",
-  ".graphql",
-  ".gql",
-  ".vue",
-  ".svelte",
+  '.js',
+  '.jsx',
+  '.mjs',
+  '.cjs',
+  '.ts',
+  '.tsx',
+  '.mts',
+  '.cts',
+  '.json',
+  '.json5',
+  '.jsonc',
+  '.html',
+  '.htm',
+  '.xml',
+  '.svg',
+  '.css',
+  '.scss',
+  '.less',
+  '.md',
+  '.markdown',
+  '.mdx',
+  '.yaml',
+  '.yml',
+  '.toml',
+  '.graphql',
+  '.gql',
+  '.vue',
+  '.svelte',
+  '.sublime-menu',
+  '.sublime-keymap',
+  '.sublime-commands'
 ];
